72013b5ed5d8216ec3330478782278a0ce9efc2d
Vojtech Brezina <vojta.brezina@gmail.com>
2021-11-30 11:33:41 +0100

    Setup a basic foundation for the loading mechanism

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,8 +1,10 @@
-extends Node2D
+extends Node
 
 onready var screen_size := get_viewport().size
 
 var font: DynamicFont
+
+var screen = 'StartScreen'
 
 func _ready():
 	font = DynamicFont.new()
@@ -10,4 +12,20 @@ func _ready():
 	font.size = 20
 	font.use_filter = false
 
-	
+	call_deferred('load_state')
+
+func load_state():
+	var file := File.new()
+	var error := file.open('user:/state.json', File.READ)
+	if not error:
+		pass
+	elif error == ERR_FILE_NOT_FOUND:
+		change_screen('HelloWorld')
+	else:
+		printerr("Error loading state.")
+		get_tree().quit()
+
+func change_screen(name):
+	if get_tree().change_scene('res://scenes/%s.tscn' % name):
+		printerr("Broken screen transition from %s to %s" % [screen, name])
+	screen = name
